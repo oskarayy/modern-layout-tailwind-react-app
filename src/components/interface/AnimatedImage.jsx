8ebab30adb2a627d42748838c6aecdf43d81d179
Oskar Czerw <oskar@o2.pl>
2023-03-22 15:42:40 +0100

    fix: bg-during-image-animation

--- src/components/interface/AnimatedImage.jsx
+++ src/components/interface/AnimatedImage.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 
 const AnimatedImage = ({ children, reverse }) => (
   <motion.div
-    style={{ zIndex: 10 }}
+    style={{ zIndex: 10, position: 'relative' }}
     initial={{ opacity: 0, translateX: reverse ? 100 : -100 }}
     whileInView={{ opacity: 1, translateX: 0 }}
     viewport={{ once: true }}
