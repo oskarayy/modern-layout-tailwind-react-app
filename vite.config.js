ffe5a6871e5ecb4d7d0ed80987aaf971a015ea71
Oskar Czerw <oskar@o2.pl>
2023-03-22 14:45:19 +0100

    --deploy-config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/modern-layout-tailwind-react-app/',
   server: {
     port: 8080,
     host: '0.0.0.0'
